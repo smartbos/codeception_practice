3cc98b0bfa6e6313adade1ef983f3a40477250cc
LeeHyunseok <smartbosslee@gmail.com>
2015-04-29 14:30:54 +0900

    js 테스트를 위한 코드 추가.

--- views/main.php
+++ views/main.php
@@ -12,8 +12,18 @@
     </div>
     <h2>Welcome</h2>
     <p><?php echo $user->email;?> 로 로그인 했습니다.</p>
-    <a href="logout.php" class="btn btn-danger">로그아웃</a>
+    <span id="logoutBtn" class="btn btn-danger">로그아웃</span>
 </div>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
+<script>
+    $('#logoutBtn').click(function(){
+        if(confirm('정말 로그아웃 하시겠습니까?')){
+            location.href = '/logout.php';
+        }
+
+        return false;
+    })
+</script>
 </body>
 </html>
