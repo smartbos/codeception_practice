7f58e610245789671f8ebda17ab66f266090573e
LeeHyunseok <smartbosslee@gmail.com>
2015-04-29 23:39:37 +0900

    selenuim 추가. siteinfo.php 수정.

--- api/siteinfo.php
+++ api/siteinfo.php
@@ -1,15 +1,23 @@
 <?php
+include_once __DIR__ . '/../vendor/autoload.php';
+
 $userId = filter_input(INPUT_GET, 'userId', FILTER_SANITIZE_STRING);
 $accessToken = filter_input(INPUT_GET, 'accessToken', FILTER_SANITIZE_STRING);
 
-$return = '';
+$result = ['msg' => 'something went wrong'];
 
-if(!$userId || !$accessToken) {
-    $return = 'Missing Parameter';
-} else {
-    if($userId != 'testUser') {
-        $return = 'Invalid User Id';
+if (!$userId || !$accessToken) {
+    $result['msg'] = 'Missing Parameter';
+    echo json_encode($result);
+    die;
+}
+
+if ($userId == 'testUser') {
+    if ($accessToken == 'testAccessToken') {
+        $result['group name'] = 'Modern PHP';
+    } else {
+        $result['msg'] = 'Invalid Access Token';
     }
 }
 
-echo json_encode($return);
+echo json_encode($result);
